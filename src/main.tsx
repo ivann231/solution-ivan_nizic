b042c39d8b34be0ccdf0bb41b61074e162123732
ivann231 <ivan.nizic02@gmail.com>
2026-07-05 20:35:29 +0200

    removed extra files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './router/router.tsx'
-import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ThemeProvider from "./styles/ThemeProvider";
 
